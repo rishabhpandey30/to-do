b791c08587dd82a20306741bfb1a0f6a2da9f6f5
Rishabh Pandey <rishabhpandey3011@gmail.com>
2025-06-15 14:27:42 +0530

    commit

---
+++ src/screens/ToDoScreen.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class ToDOScreen extends Component {
+    render() {
+        return (
+            <h1 className="ui heading center">TO-DO LIST</h1>
+        );
+    }
+}
+export default ToDOScreen;
